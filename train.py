0d4067fcd39824ff397c66d02bd1e5a18d821c0b
LeeMax117 <43522414+LeeMax117@users.noreply.github.com>
2018-12-27 01:27:10 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -7,7 +7,9 @@ import os
 import tensorflow as tf
 
 from raw_data import DataSet
-from net.test_net import get_net
+from nets import inception
+from nets.inception_v4 import inception_v4
+from nets.test_net import get_net
 
 ### to change according to your machine
 base_dir = os.path.expanduser('D:\Documents\script\python_script\AI\competation\dataset')
@@ -19,11 +21,11 @@ path_validation = os.path.join(base_dir, 'validation.h5')
 fid_validation = h5py.File(path_validation,'r')
 
 # Define loss and optimizer
-x = tf.placeholder(tf.float32, [None, 32,32,18])
+x = tf.placeholder(tf.float32, [None, 299,299,18])
 y_ = tf.placeholder(tf.float32, [None, 17])
 learning_rate = tf.placeholder(tf.float32)
 
-raw_data = DataSet(fid_validation)
+raw_data = DataSet(fid_validation,resize=True,resize_shape=[299,299])
 
 # The raw formulation of cross-entropy,
 #
@@ -32,22 +34,20 @@ raw_data = DataSet(fid_validation)
 #
 # can be numerically unstable.
 #
-# So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
-# outputs of 'y', and then average across the batch.
-y,keep_prob = get_net(x)
+y,endpoints = inception_v4(x,num_classes=17,create_aux_logits=False)
 
 cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
-l2_loss = tf.add_n([tf.nn.l2_loss(w) for w in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)])
-total_loss = cross_entropy + 7e-5 * l2_loss
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
+# l2_loss = tf.add_n([tf.nn.l2_loss(w) for w in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)])
+# total_loss = cross_entropy + 7e-5 * l2_loss
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 ## add loss and accuracy into summaries
-loss_summary = tf.summary.scalar('total_loss', total_loss)
+loss_summary = tf.summary.scalar('total_loss', cross_entropy)
 acc_summary = tf.summary.scalar('accuracy', accuracy)
 ## setup summary operation
 sum_ops_1 = tf.summary.merge([loss_summary])
@@ -62,12 +62,15 @@ summary_writer = tf.summary.FileWriter('logs', sess.graph)
 
 # Train
 # Initialized learning rate
-lr = 0.618
+lr = 0.1
 for step in range(3000):
-    batch_xs, batch_ys = raw_data.next_batch(100)
-    _, summary_1, loss, l2_loss_value, total_loss_value = sess.run(
-        [train_step, sum_ops_1, cross_entropy, l2_loss, total_loss],
-        feed_dict={x: batch_xs, y_: batch_ys, learning_rate: lr, keep_prob : 0.5})
+    batch_xs, batch_ys = raw_data.next_batch(32)
+    if raw_data.resize:
+        batch_xs = batch_xs.eval(session=sess)
+
+    _, summary_1, loss = sess.run(
+        [train_step, sum_ops_1, cross_entropy],
+        feed_dict={x: batch_xs, y_: batch_ys, learning_rate: lr})
     ## add summary 1 to file
     summary_writer.add_summary(summary_1, global_step=step)
 
