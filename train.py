56a3951448e8928762c00c12fa469c77e4a9e2d7
LeeMax117 <43522414+LeeMax117@users.noreply.github.com>
2018-12-26 15:45:18 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -43,27 +43,45 @@ l2_loss = tf.add_n([tf.nn.l2_loss(w) for w in tf.get_collection(tf.GraphKeys.TRA
 total_loss = cross_entropy + 7e-5 * l2_loss
 train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
 
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+## add loss and accuracy into summaries
+loss_summary = tf.summary.scalar('total_loss', total_loss)
+acc_summary = tf.summary.scalar('accuracy', accuracy)
+## setup summary operation
+sum_ops_1 = tf.summary.merge([loss_summary])
+sum_ops_2 = tf.summary.merge([acc_summary])
+
 sess = tf.Session()
 init_op = tf.global_variables_initializer()
 sess.run(init_op)
+
+## setup summary workspace
+summary_writer = tf.summary.FileWriter('logs', sess.graph)
+
 # Train
 # Initialized learning rate
 lr = 0.618
 for step in range(3000):
     batch_xs, batch_ys = raw_data.next_batch(100)
-    _, loss, l2_loss_value, total_loss_value = sess.run(
-        [train_step, cross_entropy, l2_loss, total_loss],
+    _, summary_1, loss, l2_loss_value, total_loss_value = sess.run(
+        [train_step, sum_ops_1, cross_entropy, l2_loss, total_loss],
         feed_dict={x: batch_xs, y_: batch_ys, learning_rate: lr, keep_prob : 0.5})
+    ## add summary 1 to file
+    summary_writer.add_summary(summary_1, global_step=step)
 
     if (step + 1) % 100 == 0:
         print('y:',y)
         print('step %d, entropy loss: %f, l2_loss: %f, total loss: %f' %
               (step + 1, loss, l2_loss_value, total_loss_value))
         # Test trained model
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print(sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys, keep_prob:0.5}))
+        _, summary_2 = sess.run([accuracy, sum_ops_2], feed_dict={x: batch_xs, y_: batch_ys, keep_prob:0.5})
+        ## add summary 2 to file
+        summary_writer.add_summary(summary_2, global_step=step)
 
     # learning rate dacey
     if (step + 1) % 300 == 0:
         lr = 0.618 * lr
+        
+summary_writer.close()
