578f3caa200a683acf38e714ff401d4307548872
LeeMax117 <43522414+LeeMax117@users.noreply.github.com>
2019-01-02 01:52:43 +0800

    Add files via upload

--- nomarlize_dataset.py
+++ nomarlize_dataset.py
@@ -2,12 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
 
 def add_image(image,sum,s_var):
-    for i in image:
-        sum = sum + i
-        s_var = s_var + i*i
+    for row in image:
+        for i in row:
+            sum = sum + i
+            s_var = s_var + i*i
     return sum, s_var
 
 def get_avg_var(data):
@@ -21,4 +21,46 @@ def get_avg_var(data):
 
     for per_data in data:
         for channel_num in range(0,per_data.shape[2]):
-            layer_sum[channel_num], layer_s_var[channel_num] = add_image(per_data[:,:,channel_num])
+            layer_sum[channel_num], layer_s_var[channel_num] = \
+                add_image(per_data[:,:,channel_num],layer_sum[channel_num], layer_s_var[channel_num])
+
+    layer_avg , layer_var = [],[]
+    sum_var = []
+    for i in range(0, data.shape[3]):
+        layer_avg.append(layer_sum[i] / data.shape[0])
+        layer_var.append((layer_s_var[i] - layer_avg[i]*layer_avg[i]) / (data.shape[0]-1))
+        sum_var.append(layer_s_var[i]/ (data.shape[0]-1))
+
+    print('sum_var:',sum_var)
+
+    return layer_avg , layer_var
+
+
+
+
+
+
+'''from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import h5py
+import os
+from nomarlize_dataset import get_avg_var
+
+### to change according to your machine
+base_dir = os.path.expanduser('D:\Documents\script\python_script\AI\competation\dataset')
+# path_training = os.path.join(base_dir, 'training.h5')
+path_validation = os.path.join(base_dir, 'validation.h5')
+
+# print(path_training)
+#fid_training = h5py.File(path_training,'r')
+fid_validation = h5py.File(path_validation,'r')
+
+print(fid_validation['sen1'].shape)
+
+avg,var = get_avg_var(fid_validation['sen1'])
+
+
+print('average for each channel is ',avg)
+print('var for each channel is ' ,var)'''
--- train.py
+++ train.py
@@ -11,13 +11,19 @@ from nets import nets_factory
 
 slim = tf.contrib.slim
 
+###########################################
+##### set the parameter of the system######
+###########################################
+
 ### to change according to your machine
 base_dir = os.path.expanduser('D:\Documents\script\python_script\AI\competation\dataset')
 # path_training = os.path.join(base_dir, 'training.h5')
 path_validation = os.path.join(base_dir, 'validation.h5')
-
 ### set the checkpoint path
 ckpt_folder = 'model'
+finetune_ckpt = None
+# define the process of trainning or validation
+is_trainning = True
 
 # print(path_training)
 #fid_training = h5py.File(path_training,'r')
@@ -27,6 +33,7 @@ fid_validation = h5py.File(path_validation,'r')
 x = tf.placeholder(tf.float32, [None, 32,32,18])
 y_ = tf.placeholder(tf.float32, [None, 17])
 learning_rate = tf.placeholder(tf.float32)
+global_step = tf.Variable(0, name='step')
 
 # raw_data = DataSet(fid_validation,resize=True,resize_shape=[299,299])
 raw_data = DataSet(fid_validation)
@@ -42,7 +49,7 @@ network_fn = nets_factory.get_network_fn(
     'M_inception_v4',
     num_classes=17,
     weight_decay=0.0001,
-    is_training=True)
+    is_training=is_trainning)
 
 y , end_points = network_fn(x)
 
@@ -71,43 +78,59 @@ sess.run(init_op)
 summary_writer = tf.summary.FileWriter('logs', sess.graph)
 
 ## save the whole NN model into checkpoint path
-saver = tf.train.Saver()
+saver = tf.train.Saver(max_to_keep=3)
+
+if finetune_ckpt:
+    saver.restore(sess, finetune_ckpt)
+else:
+    model_file = tf.train.latest_checkpoint(ckpt_folder)
+    try:
+        saver.restore(sess, model_file)
+    except ValueError:
+        if os.path.isdir(ckpt_folder):
+            print('trainning from beginning')
 
 # Train
-# Initialized learning rate
-lr = 0.01
-for step in range(300):
+if is_trainning:
+    # Initialized learning rate
+    lr = 0.01
+    for step in range(300):
+        batch_xs, batch_ys = raw_data.next_batch(12)
+
+        global_step = global_step + 1
+        # if use resize, need to transer tensor to numpy
+        if raw_data.resize:
+            batch_xs = batch_xs.eval(session=sess)
+
+        _, summary_1, loss, g_step = sess.run(
+            [train_step, sum_ops_1, cross_entropy, global_step],
+            feed_dict={x: batch_xs, y_: batch_ys, learning_rate: lr})
+
+        print('step %d, entropy loss: %f' %
+              (g_step, loss))
+
+        if (step + 1) % 2 == 0:
+            ## add summary 1 to file
+            summary_writer.add_summary(summary_1, global_step=step)
+
+            # save ckpt
+            ckpt_path = os.path.join(ckpt_folder, 'model.ckpt')
+            saver.save(sess, ckpt_path , global_step=step)
+            print('Model saved in path %s' % ckpt_path)
+            # Test trained model
+            # acc, summary_2 = sess.run([accuracy, sum_ops_2], feed_dict={x: batch_xs, y_: batch_ys})
+            # print(acc)
+            ## add summary 2 to file
+            #summary_writer.add_summary(summary_2, global_step=step)
+            summary_writer.close()
+
+        # learning rate dacey
+        if (step + 1) % 30 == 0:
+            lr = 0.618 * lr
+else:
+    correct_prediction = tf.equal(tf.argmax(logit, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
     batch_xs, batch_ys = raw_data.next_batch(24)
-
-    # if use resize, need to transer tensor to numpy
-    if raw_data.resize:
-        batch_xs = batch_xs.eval(session=sess)
-
-    _, summary_1, loss = sess.run(
-        [train_step, sum_ops_1, cross_entropy],
-        feed_dict={x: batch_xs, y_: batch_ys, learning_rate: lr})
-
-    print('step %d, entropy loss: %f' %
-          (step + 1, loss))
-
-
-
-    if (step + 1) % 30 == 0:
-        ## add summary 1 to file
-        summary_writer.add_summary(summary_1, global_step=step)
-
-        # save ckpt
-        ckpt_path = os.path.join(ckpt_folder, 'model.ckpt-%d' % (step + 1))
-        saver.save(sess, ckpt_path)
-        print('Model saved in path %s' % ckpt_path)
-        # Test trained model
-        # acc, summary_2 = sess.run([accuracy, sum_ops_2], feed_dict={x: batch_xs, y_: batch_ys})
-        # print(acc)
-        ## add summary 2 to file
-        #summary_writer.add_summary(summary_2, global_step=step)
-
-    # learning rate dacey
-    if (step + 1) % 30 == 0:
-        lr = 0.618 * lr
-        
-summary_writer.close()
+    acc = sess.run(accuracy, feed_dict={placeholder: batch_xs, y_: batch_ys})
+    print(acc)
