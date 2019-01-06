938f505be6704ecae234bb78eddf98b49806acc6
LeeMax117 <43522414+LeeMax117@users.noreply.github.com>
2019-01-07 01:47:56 +0800

    Add files via upload

--- nets/densenet.py
+++ nets/densenet.py
@@ -172,7 +172,11 @@ def bn_drp_scope(is_training=True, keep_prob=0.8):
             return bsc
 
 
-def densenet_arg_scope(weight_decay=0.004):
+def densenet_arg_scope(weight_decay=0.00004,
+                        use_batch_norm=True,
+                        batch_norm_decay=0.9997,
+                        batch_norm_epsilon=0.001,
+                        activation_fn=tf.nn.relu):
     """Defines the default densenet argument scope.
 
     Args:
@@ -181,13 +185,32 @@ def densenet_arg_scope(weight_decay=0.004):
     Returns:
       An `arg_scope` to use for the inception v3 model.
     """
-    with slim.arg_scope(
-        [slim.conv2d],
-        weights_initializer=tf.contrib.layers.variance_scaling_initializer(
-            factor=2.0, mode='FAN_IN', uniform=False),
-        activation_fn=None, biases_initializer=None, padding='same',
-            stride=1) as sc:
-        return sc
+    batch_norm_params = {
+        # Decay for the moving averages.
+        'decay': batch_norm_decay,
+        # epsilon to prevent 0s in variance.
+        'epsilon': batch_norm_epsilon,
+        # collection containing update_ops.
+        'updates_collections': tf.GraphKeys.UPDATE_OPS,
+        # use fused batch norm if possible.
+        'fused': None,
+    }
+    if use_batch_norm:
+        normalizer_fn = slim.batch_norm
+        normalizer_params = batch_norm_params
+    else:
+        normalizer_fn = None
+        normalizer_params = {}
+    # Set weight_decay for weights in Conv and FC layers.
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        weights_regularizer=slim.l2_regularizer(weight_decay)):
+        with slim.arg_scope(
+                [slim.conv2d],
+                weights_initializer=slim.variance_scaling_initializer(),
+                activation_fn=activation_fn,
+                normalizer_fn=normalizer_fn,
+                normalizer_params=normalizer_params) as sc:
+            return sc
 
 
 densenet.default_image_size = 224
